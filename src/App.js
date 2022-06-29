fefe06883414f75df72142d93038868440edb852
Sandivsv <sandeepkmr7379@gmail.com>
2022-06-29 17:45:59 +0530

    Routing implemented for sub-page via Link

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import './App.css';
-// import Banner from './components/Banner';
-// import MovieList from './components/movieList';
+import Banner from './components/Banner';
+import MovieList from './components/movieList';
 import NavBar from './components/NavBar';
 import Fav from './components/Fav'
+import {BrowserRouter,Routes,Route, Router} from 'react-router-dom'
+
 
 
 function App() {
   return (
     <>
+      <BrowserRouter>
       <NavBar/>
-      {/* <Banner/>
-      <MovieList/> */}
-      <Fav/>
+      <Routes>
+        <Route path="/" element={<><Banner/><MovieList/></>}/>
+        <Route path="/favourites" element={<Fav/>}/>
+      </Routes>
+    </BrowserRouter>
     </>
     );
 }
