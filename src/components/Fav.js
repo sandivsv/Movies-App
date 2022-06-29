22ddc3b018414e3b2be86ab3497ef7da90d0b96a
Sandivsv <sandeepkmr7379@gmail.com>
2022-06-29 16:17:34 +0530

    implement category based movie tag

--- src/components/Fav.js
+++ src/components/Fav.js
@@ -1,26 +1,76 @@
+// to ignore dom related warning install ->      npm i react-router-dom
+
 import { Component } from "react";
+import { movies } from '../movieData';
 
 
 class Fav extends Component {
 
     render() {
+
+        const moviesArr = movies.results;
+        console.log(moviesArr)
+        let genreIds = { 28: "Action", 12: "Adventure", 16: "Animation", 35: "Comedy", 80: "Crime", 99: "Documentary", 18: "Drama", 10751: "Family", 14: "Fantasy", 36: "History", 27: "Horror", 10402: "Music", 9648: "Mystery", 10749: "Romance", 878: "Science Fiction", 10770: "TV Movie", 53: "Thriller", 10752: "War", 37: "Western"}
+        
+        let tempArr = [];
+        tempArr.push("All genres");
+        // eslint-disable-next-line
+        moviesArr.map((movieObj) => {
+            if(!tempArr.includes(genreIds[movieObj.genre_ids[0]])){
+                tempArr.push(genreIds[movieObj.genre_ids[0]])
+            }
+        });
+
         return (
             <div className="container">
                 <div className="row">
                     <div className="col-3">
                         <ul className="list-group genre-selector">
-                            <li className="list-group-item">Favourites</li>
-                            <li className="list-group-item">Action</li>
-                            <li className="list-group-item">Action</li>
-                            <li className="list-group-item">Comedy</li>
-                            <li className="list-group-item">Adventure</li>
+                            {
+                                tempArr.map((genre)=>(
+                                    <li className="list-group-item">{genre}</li>
+                                ))
+                            }
                         </ul>
                     </div>
                     <div className="col-9 fav-table">
                         <div className="row">
-                            <input type="text" className="form-control" placeholder="Search"/>
-                            <input type="number" className="form-control" />
-                        </div>                  
+                        <input type="text" className="form-control col" placeholder="Search" />
+                            <input type="number" className="form-control col" />
+                        </div>
+
+                        <table className="table">
+                            <thead>
+                                <tr>
+                                    <th scope="col">Title</th>
+                                    <th scope="col">Genre</th>
+                                    <th scope="col">Popularity</th>
+                                    <th scope="col">Rating</th>
+                                    <th scope="col">Delete</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {
+                                    moviesArr.map((movieEle)=>(
+                                        <tr>
+                                            <th scope="row"><img alt="poster" style={{ width: "8rem", padding: "1rem" }} src={`https://image.tmdb.org/t/p/original${movieEle.backdrop_path}`} />{movieEle.title}</th>
+                                            <td>{genreIds[movieEle.genre_ids[0]]}</td>
+                                            <td>{movieEle.popularity}</td>
+                                            <td>{movieEle.vote_average}</td>
+                                            <td><button type="button" className="btn btn-danger">Danger</button></td>
+                                        </tr>
+                                    ))
+                                }
+                            </tbody>
+                        </table>
+
+                        <nav aria-label="Page navigation example">
+                            <ul className="pagination">
+                                <li className="page-item"><a className="page-link" href=" ">1</a></li>
+                                <li className="page-item"><a className="page-link" href=" ">2</a></li>
+                                <li className="page-item"><a className="page-link" href=" ">3</a></li>
+                            </ul>
+                        </nav>                
                     </div>
                 </div>
             </div>
@@ -29,4 +79,4 @@ class Fav extends Component {
 }
 
 
-export default Fav;
+export default Fav
